764c28ccf9c0adaa8909a53760ccfbda6094e3c0
Aishwarya Gharat <71493143+aishwarya220@users.noreply.github.com>
2021-04-10 16:07:41 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,44 @@
+
+<!-- navbar --> 
+
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+
+      <a class="navbar-brand" href="index.php"><h2>SPARKS BANK</h2></a>
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+
+        <span class="navbar-toggler-icon"></span>
+
+      </button>
+
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+
+            <ul class="navbar-nav ml-auto">
+
+              <li class="nav-item">
+
+                <a class="nav-link" href="index.php">Home</a>
+
+              </li>
+
+              <li class="nav-item">
+
+                <a class="nav-link" href="createuser.php">Create User</a>
+
+              </li>
+
+              <li class="nav-item">
+
+                <a class="nav-link" href="transfermoney.php">Money Transfer</a>
+
+              </li>
+
+              <li class="nav-item">
+
+                <a class="nav-link" href="transactionhistory.php">Transaction History</a>
+
+              </li>
+
+          </div>
+
+       </nav>
